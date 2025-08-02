164b28b5079c3d773ea45a2562e7fdea0a91dd4a
LucasRossatto <lucas.sant.anagn3@gmail.com>
2025-08-02 12:46:27 -0300

    add: novos graficos relacionados a Ticket medio

--- aula4/stream.py
+++ aula4/stream.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -7,11 +6,23 @@ df = pd.read_csv("aula4/vendas_loja.csv")
 df["Receita"] = df["Quantidade"] * df["Preço_unitario"]
 df["Data"] = pd.to_datetime(df["Data"])
 df["Mês"] = df["Data"].dt.to_period("M")
-
+df["Ticket-medio"] = df["Receita"] / df["Quantidade"] 
 st.title("Dashboard de vendas")
+st.markdown(
+    """ 
+    Bem vindo a Dashboard de vendas, feita em Streamlit e pands 
+
+    **There's  you can build!**
+    
+    We prepared a few examples for you to get started. Just 
+    click on the buttons above and discover what you can do 
+    with Streamlit. 
+    """
+)
 
 st.metric("Total de vendas", f"R$ {df["Receita"].sum():,.2f}")
 st.metric("Média por venda", f"R$ {df['Receita'].mean():,.2f}")
+st.metric("Ticket Médio", f"R$ {df['Ticket-medio'].mean():,.2f}")
 
 categorias = df["Categoria"].unique()
 categoria_selecionada = st.selectbox("Selecione a categoria", categorias)
@@ -19,13 +30,40 @@ df_filtrado = df[df["Categoria"] == categoria_selecionada]
 
 # Grafico por produto
 st.subheader("Receita por produto")
-# criar uma figura
 fig1, ax1 = plt.subplots()
 df_filtrado.groupby("Produto")["Receita"].sum().plot(kind="bar", ax=ax1)
 st.pyplot(fig1)
 
-# Grafico por mes
+st.subheader("Ticket Médio por categoria")
+fig4, ax4 = plt.subplots()
+df_filtrado.groupby("Produto")["Ticket-medio"].sum().plot(kind="bar", ax=ax4)
+st.pyplot(fig4)
+
+
+st.write("Ticket médio por produto")
+todos_produtos = df["Produto"].unique().tolist()
+with st.container(border=True):
+    produtos = st.multiselect("Produtos", todos_produtos, default=todos_produtos)
+
+df_filtrado = df[df["Produto"].isin(produtos)].copy()
+df_filtrado["Ticket-medio"] = df_filtrado["Receita"] / df_filtrado["Quantidade"]
+
+ticket_produto = df_filtrado.groupby("Produto")["Ticket-medio"].mean().reset_index()
+ticket_produto = ticket_produto.set_index("Produto")
+# Formatado para R$
+ticket_produto["Ticket-medio"] = ticket_produto["Ticket-medio"].apply(lambda x: f"R$ {x:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
+
+
+tab1, tab2 = st.tabs(["📈 Gráfico", "📋 Tabela"])
+tab1.line_chart(ticket_produto, height=250)
+tab2.dataframe(ticket_produto, height=250, use_container_width=True)
+
 st.subheader("Receita mensal")
 fig2, ax2 = plt.subplots()
 df.groupby("Mês")["Receita"].sum().plot(ax=ax2)
 st.pyplot(fig2)
+
+st.subheader("Ticket Médio mensal")
+fig3, ax3 = plt.subplots()
+df.groupby("Mês")["Ticket-medio"].sum().plot(ax=ax3)
+st.pyplot(fig3)
